ccbc8642c21c541eede43af1cb7b9c3ee6dd52b4
unknown <manvirm29@gmail.com>
2022-02-02 23:33:02 -0800

    add button

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,6 +1,7 @@
 //import './App.css';
 import React from 'react'
 import Header from './components/Header'
+import { useState } from 'react'
 function App() {
 
   //can only return a single element, 
---
+++ react-task-tracker/src/components/Task.js
@@ -0,0 +1,8 @@
+import { useState } from 'react'
+
+const Task = () => {
+   
+  return <div></div>;
+};
+
+export default Tasks
